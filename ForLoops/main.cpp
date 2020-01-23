021f33fd809b1e877ee4446c7676a006566344f8
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-01-23 09:47:50 -0300

    Add incomplete, basic for loops examples

---
+++ ForLoops/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+//	cout << "###### Couting up to 10" << endl;
+//	for  (int i{1}; i <= 10; ++i)
+//		cout << i << endl;
+	
+	
+//	cout << "\n###### Couting up to 10 by 2" << endl;
+//	for  (int i{1}; i <= 10; i += 2)
+//		cout << i << endl;
+	
+	
+//	cout << "\n###### Couting down from 10" << endl;
+//	for  (int i{10}; i > 0; --i)
+//		cout << i << endl;
+//	cout << "Blastoff!" << endl;
+	
+	
+//	cout << "\n###### Couting from 10 to  100 by 10, but printing only divisibles by 15" << endl;
+//	for  (int i{10}; i <= 100; i += 10){
+//		if (i % 15 == 0)
+//			cout << i << endl;
+//	}
+	
+	
+	cout << "\n###### Using comma to have more than one loop variable" << endl;
+	for (int i{1}, j{5}; i <= 5; ++i, ++j){
+		cout << i << " + " << j << " = " << i+j << endl;
+	}
+	
+	cout << "\n###### Printing a table" << endl;
+	for (int i{1}; i <= 100; ++i){
+		cout << i;
+		if (i % 10 == 0)
+			cout << endl;
+		else
+			cout << " ";
+	}
+	
+	
+	cout << "\n###### Printing a table in one line using conditional operator" << endl;
+	for (int i{1}; i <= 100; ++i){
+		cout << i << ((i % 10 == 0) ? "\n" : " ");
+	}
+	
+	
+	cout << "\n###### My nicely spaced table << endl;
+	for (int i{1}; i <= 100; ++i){
+		if (i < 10)
+			cout << "00" << i;
+		else if (i < 100)
+			cout << "0" << i;
+		else
+			cout << i;
+		
+		cout << ((i % 10 == 0) ? "\n" : " ");
+	}
+	
+	
+	return 0;
+}
--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: clean All
 
 All:
-	@echo "----------Building project:[ ConditionalOperator - Debug ]----------"
-	@cd "ConditionalOperator" && "$(MAKE)" -f  "ConditionalOperator.mk"
+	@echo "----------Building project:[ ForLoops - Debug ]----------"
+	@cd "ForLoops" && "$(MAKE)" -f  "ForLoops.mk"
 clean:
-	@echo "----------Cleaning project:[ ConditionalOperator - Debug ]----------"
-	@cd "ConditionalOperator" && "$(MAKE)" -f  "ConditionalOperator.mk" clean
+	@echo "----------Cleaning project:[ ForLoops - Debug ]----------"
+	@cd "ForLoops" && "$(MAKE)" -f  "ForLoops.mk" clean
