e9e223089eb9e9cc9a0f6cb69a63b4cb938d2a7e
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-01-24 23:10:36 -0300

    Finish loop examples

--- ForLoops/main.cpp
+++ ForLoops/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -48,7 +49,7 @@ int main(){
 	}
 	
 	
-	cout << "\n###### My nicely spaced table << endl;
+	cout << "\n###### My nicely spaced table" << endl;
 	for (int i{1}; i <= 100; ++i){
 		if (i < 10)
 			cout << "00" << i;
@@ -58,6 +59,12 @@ int main(){
 			cout << i;
 		
 		cout << ((i % 10 == 0) ? "\n" : " ");
+	}
+	
+	cout << "\n###### Loop through vectors" << endl;
+	vector<int> nums {10,20,30,40,50};
+	for (unsigned i{0}; i<nums.size(); ++i){
+		cout << nums[i] << endl;
 	}
 	
 	
