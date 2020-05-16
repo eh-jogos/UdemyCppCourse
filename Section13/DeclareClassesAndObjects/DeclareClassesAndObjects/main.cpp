11665892cc74be35a70c397290d0f879b0d29076
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-05-16 19:49:09 -0300

    Start of Section 13

---
+++ Section13/DeclareClassesAndObjects/DeclareClassesAndObjects/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+class Player{
+	//attributes
+	string name;
+	int health;
+	int xp;
+	
+	//methods
+	void talk(string msg);clTabCtrl
+	bool is_dead();
+};
+
+
+int main()
+{
+	Player frank;
+	Player hero;
+	
+	cout << "Please stop here " << endl; 
+	return 0;
+}
